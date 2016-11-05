b1784653702e4066a9b63c03377515ab8062ec15
maldonado93 <jorgemaldonad12@hotmail.com>
2016-11-05 11:56:07 -0500

    sql

---
+++ FundacionProNiñez.sql
@@ -0,0 +1,17 @@
+﻿-- Table: participa
+
+-- DROP TABLE participa;
+
+CREATE TABLE participa
+(
+  proyecto integer NOT NULL,
+  tiempo character varying(20),
+  tarea character varying(40),
+  trabajador character varying(40) NOT NULL,
+  CONSTRAINT participa_pkey PRIMARY KEY (proyecto, trabajador)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE participa
+  OWNER TO postgres;
